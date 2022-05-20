ffb51b4b369a997d7d41aaea533d5ad551ba22e6
speedbuild98 <lautaropectm98@gmail.com>
2022-05-20 15:49:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import QR from './images/qr.png';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='Contenedor'>
+        <img className='imgQR' src={QR}/>
+        <h1>Improve your front-end<br/>skills by building projects</h1>
+        <p>Scan the QR code to visit Frontend<br/>Mentor and take your coding skills to<br/>the next level</p>
+      </div>
+      <div className="attribution">
+      Challenge by <a rel="noreferrer" href="https://www.frontendmentor.io/profile/speedbuild98" target="_blank">Frontend Mentor</a>. 
+      Coded by <a rel="noreferrer" href="https://devgallardo.netlify.app/">GALLARDO</a>.
+      </div>
     </div>
+    
   );
 }
 
